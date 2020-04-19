4d8ad6f46ec8c815ab8a6603dc8028bee4c58aa3
yanbaru917 <ttaakkeemsss@gmail.com>
2020-04-19 22:27:31 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+  end
+end
