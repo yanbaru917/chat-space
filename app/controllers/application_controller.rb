75823c40eae5a2a316c64c55483a778aa6b66a5c
yanbaru917 <ttaakkeemsss@gmail.com>
2020-04-22 21:34:08 +0900

    add_dvise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_paramaters, if: :devise_controller?
 end
