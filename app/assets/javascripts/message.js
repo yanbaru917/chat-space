aa1a969f3e3be8d1585ca1e9661165871d931ea7
yanbaru917 <ttaakkeemsss@gmail.com>
2020-04-28 17:27:33 +0900

    create-autmatic-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
     if  (message.image) {
       var html = 
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__upper-info">
             <p class="message__upper-info__talker"> 
               ${message.user_name}
@@ -15,13 +16,13 @@ $(function(){
             <p class="message__text__content">
               ${message.content}
             </p> 
-            <img src=${message.image}
+            <img src=${message.image} >
           </div>
         </div>`
       return html; 
     } else {
       var html = 
-        `<div class="message"> 
+        `<div class="message" data-message-id=${message.id}> 
           <div class="message__upper-info">
             <p class="message__upper-info__talker">
               ${message.user_name}
@@ -39,8 +40,6 @@ $(function(){
       return html;
     };
   }
-
-
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -60,10 +59,36 @@ $(function(){
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         $('form')[0].reset();
         $('input').prop('disabled', false);
-
       })
       .fail(function(){
         alert("メッセージの送信に失敗しました");
       })
-  })
+
+    });
+  let reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[id])
+    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
